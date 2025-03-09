ec0b54c5af684b801258c6ce7e01b5217369f651
Vera Mstislavtseva <mstislavtsevavv@gmail.com>
2025-03-09 13:23:18 +0300

    feat(cases): добавить секцию "кейсы", добавить слайдер

---
+++ js/swiper.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 20,
+  freeMode: true,
+  resistance: false,
+  allowTouchMove: true,
+  centeredSlides: false,
+
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
